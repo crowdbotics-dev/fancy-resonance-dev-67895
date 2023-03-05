7d7d6d396a6fccb02affab1d70afd1b0c30655bc
Crowdbotics <engineering@crowdbotics.com>
2023-03-05 01:54:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fancy_resonance_dev_67895/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fancy_resonance_dev_67895;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "fancy_resonance_dev_67895";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-resonance-dev-67895",
+  url: "https://fancy-resonance-dev-67895.botics.co",
+  api: "https://fancy-resonance-dev-67895.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
